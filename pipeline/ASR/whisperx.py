fd610287dab81435bd56980c833ba36d0bb1b4a7
lukin921 <lukin921@student.liu.se>
2025-10-10 15:17:34 +0200

    added (limited) baseline test for whisperX ASR

--- pipeline/ASR/whisperx.py
+++ pipeline/ASR/whisperx.py
@@ -1,11 +1,23 @@
-import whisperx
+from utils import load_audio_from_file, match_frequency 
 
-# TODO: Specify types for function/class args
+import whisperx
+import torch
 
 class WhsiperXASR:
-    def __init__(self, model="base", device="cuda", compute_type="float16"):
-        self.model = whipserx.load_model(model, device, compute_type=compute_type)
+    def __init__(self, model: str = "base", device: str = "cuda", compute_type: str = "float32") -> None:
+        self.model = whisperx.load_model(model, device, compute_type=compute_type)
 
-    def transcribe(self, audio, batch_size):
-        result = self.model.transcribe(audio, batch_size=batch_size)
-        return result
+    def transcribe(self, audio: torch.Tensor, batch_size: int) -> list[dict[str, str]]:
+        result = self.model.transcribe(audio.numpy(), batch_size=batch_size)
+        segments = result["segments"]
+        return segments
+
+
+if __name__ == "__main__":
+    asr = WhsiperXASR(device="cpu")
+    file_path = "single_speaker_sample.wav"
+    audio, freq = load_audio_from_file(file_path)
+    audio = match_frequency(audio, freq)
+    segments = asr.transcribe(audio, batch_size=1)
+    print(segments)
+
